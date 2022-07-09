e1a7f6f28ba611a3c404ded09dc5b151aeffb5f6
Igor Maknickas <residentevil.q2@mail.ru>
2022-07-10 01:32:52 +0300

    task8

---
+++ task_8/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая вводит (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("введите число");
+int a = int.Parse(Console.ReadLine());
+for ( int i = 2; i <= a; i++)
+if (i % 2 == 0)
+Console.WriteLine(" Четные числа = " + i);
